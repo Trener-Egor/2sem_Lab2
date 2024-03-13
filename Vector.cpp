0f3b2ea48b9039124cc05d657111cf1b781df2dd
Trener-Egor <243593444@mail.ru>
2024-03-13 17:46:15 +0300

    done

---
+++ Vector.cpp
@@ -0,0 +1,196 @@
+#include "Vector.hpp"
+
+
+
+
+
+vector::vector(double* arr, int len)
+{
+	this->dim_ = len;
+	this->data_ = new double[len];
+	for (int i = 0; i < len; ++i)
+	{
+		this->data_[i] = arr[i];//*(data_+i)=*(arr+i)
+	}
+}
+
+vector::vector( int dim)
+{
+	dim_ = dim;
+	this->data_ = new double[dim];
+}
+//vector a(d, 3);
+//vector b(a);
+
+vector::vector(const vector& other)
+{
+	if (this != &other)
+	{
+		dim_ = other.dim_;
+		data_ = new double[dim_];
+		for (int i = 0; i < dim_; ++i)
+		{
+			this->data_[i] = other.data_[i];
+		}
+	}
+}
+//vector a(d, 3);
+//vector b(std::move(a));
+vector::vector(vector&& other)
+{
+	if (this != &other)
+	{
+		dim_ = other.dim_;
+		data_ = other.data_;
+		other.data_ = nullptr;
+	}
+}
+
+vector::~vector()
+{
+	if (data_ != nullptr)
+	{
+		delete[] data_;
+	}
+}
+
+//vector a(d, 3);
+//vector b=a;
+vector& vector::operator=(const vector& other)
+{
+
+	if (this != &other)
+	{
+		dim_ = other.dim_;
+		data_ = new double[dim_];
+		for (int i = 0; i < dim_; ++i)
+		{
+			this->data_[i] = other.data_[i];
+		}
+	}
+	return *this;
+}
+
+//vector a(d, 3);
+//vector b=std::move(a);
+vector& vector::operator=(vector&& other)
+{
+	if (this != &other)
+	{
+		dim_ = other.dim_;
+		data_ = other.data_;
+		other.data_ = nullptr;
+	}
+	return *this;
+}
+
+double&       vector::operator[](int idx)
+{
+	return data_[idx];
+}
+const double& vector::operator[](int idx) const
+{
+	return data_[idx];
+}
+
+int vector::get_dim() const
+{
+	return dim_;
+}
+
+std::istream& operator>>(std::istream& is, vector& o)
+{
+	for(int i=0;i<o.dim_;++i)
+	{
+		is >> o.data_[i];
+	}
+	return is;
+}
+std::ostream& operator<<(std::ostream& os, const vector& o)
+{
+	for (int i = 0; i < o.dim_; ++i)
+	{
+		os << o.data_[i] << ' ';
+		
+	}
+	return os;
+}
+
+
+//постфиксный декремент
+const vector vector::operator--(int)
+{
+	vector old(dim_);
+	for(int i=0;i<dim_;++i)
+	{
+		old.data_[i] = data_[i];
+		data_[i]--;
+	}
+	return old;
+}
+//префиксный декремент
+vector& vector::operator--()
+{
+	for (int i = 0; i < dim_; ++i)
+	{
+		
+		data_[i]--;
+	}
+	return *this;
+}
+
+
+//постфиксный инкремент
+const vector vector::operator++(int)
+{
+	vector old(dim_);
+	for (int i = 0; i < dim_; ++i)
+	{
+		old.data_[i] = data_[i];
+		data_[i]++;
+	}
+	return old;
+}
+//префиксный инкремент
+vector& vector::operator++()
+{
+	for (int i = 0; i < dim_; ++i)
+	{
+
+		data_[i]++;
+	}
+	return *this;
+}
+
+
+vector       vector::operator*(double c) const 
+{
+	vector result(dim_);
+	for (int i = 0; i < dim_; ++i)
+	{
+		result.data_[i] = data_[i] * c;
+	}
+	return result;
+}
+double       vector::operator*(const vector& other) const
+{
+	double result = 0.0;
+	for (int i = 0; i < dim_; ++i)
+	{
+		result += data_[i] * other.data_[i];
+	}
+	return result;
+}
+vector operator+(const vector& a, double* b)
+{
+	vector result(a.get_dim() * 2);
+	for (int i = 0; i < a.get_dim(); ++i)
+	{
+		result[i] = a[i];
+	}
+	for (int i = 0; i < a.get_dim(); ++i)
+	{
+		result[i + a.get_dim()] = b[i];
+	}
+	return result;
+}
---
+++ lab 2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <fstream>
+#include "Vector.hpp"
+
+int main()
+{   
+    /*std::cout<<sizeof vector<< "\n";
+    vector a(3);
+    std::cin >> a;
+    std::cout << "a: " << a << "\n";
+    vector b(a);
+    std::cin >> b;
+    std::cout << "b: " << b << "\n";
+    vector c = b;
+    std::cout << "c: " << c << "\n";
+    vector d = std::move(c);
+    c.~vector();
+    std::cout << "d: " << d << "\n";*/
+
+    std::ifstream file("input.txt");
+
+    if(!file.is_open())
+    {
+        std::cout << "errror: file not open \n";
+        return 1;
+    }
+
+    int dim;
+    file >> dim;
+    vector v1(dim);
+    vector v2(dim);
+    for(int i =0;i<dim;++i)
+    {
+        file >> v1[i];
+    }
+    for (int i = 0; i < dim; ++i)
+    {
+        file >> v2[i];
+    }
+    std::cout << "vector1 :\n";
+    std::cout << v1 << "\n";
+
+    std::cout << "vector2 :\n";
+    std::cout << v2 << "\n";
+    double sk = v1 * v2;
+    std::ofstream out("out.txt");
+
+    if (!out.is_open())
+    {
+        std::cout << "errror: file not open \n";
+        return 1;
+    }
+    out << "dot product: " << sk << "\n";
+}
