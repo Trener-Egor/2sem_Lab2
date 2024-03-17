89af6319be7a68f1a1e4835a13f5f2c5bebfdb18
Trener-Egor <148523483+Trener-Egor@users.noreply.github.com>
2024-03-17 16:28:26 +0300

    Update Vector.hpp

--- Vector.hpp
+++ Vector.hpp
@@ -2,58 +2,37 @@
 #define VECTOR_HPP
 
 #include <iostream>
-
 class vector
 {
 public:
 	vector(double* arr, int len);
 	vector(int dim);
 
+	vector(const vector& other);//copy ctr
+	vector(vector&& other);//move ctr
+	vector& operator=(const vector& other);//copy assigment
+	vector& operator=(vector&& other);//move assigment
+
 	~vector();
 
-	vector(const vector& other);
-	vector(vector&& other);
-
-
-	vector& operator=(const vector& other);
-	vector& operator=(vector&& other);
-
 	      double&       operator[](int idx);
-	const double&       operator[](int idx) const;
+	const double&       operator[](int idx)const;
 
 	int get_dim() const;
 
 	friend std::istream& operator>>(std::istream& is, vector& o);
 	friend std::ostream& operator<<(std::ostream& os, const vector& o);
+
 	const vector operator--(int);
 	vector&      operator--();
 	const vector operator++(int);
 	vector&      operator++();
 	vector       operator*(double c) const;
-	double       operator*(const vector& other) const;
+	double       operator*(const vector& other) const;	
+	vector       operator+ (double* v2);
 private:
 	double* data_;
 	int     dim_;
 };
-
-//Vector	double *	Vector
-//vector operator+ (double* v2,const vector& v1)
-//{	
-//
-//	return v1+v2;//v1.operator+(v2);
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif // VECTOR_HPP
